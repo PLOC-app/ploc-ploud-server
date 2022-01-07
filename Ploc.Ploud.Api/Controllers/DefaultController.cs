7ba7d1741516d747c6074ea2ecf40cc2bddfb710
PLOC <70281334+ploc-app-des-epicuvins@users.noreply.github.com>
2022-01-07 17:18:23 +0100

    add default response

---
+++ Ploc.Ploud.Api/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ploc.Ploud.Api.Controllers
+{
+    [Route("")]
+    [Route("v1")]
+    [ApiController]
+    public class DefaultController : ApiControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var status = new
+            {
+                Status = Config.Success,
+                Feedback = "lapaire@PLOC.co",
+                Github = "https://github.com/PLOC-app/ploc-ploud-server/",
+                Website = "https://www.PLOC.co/"
+            };
+            return Ok(status);
+        }
+    }
+}
